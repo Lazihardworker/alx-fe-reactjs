6589fb4b5f978fbd5a9cad443aba0ad008094b40
suleboss <walesolih@gmail.com>
2025-07-14 00:56:26 +0100

    Adding the header,main content and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -48,7 +48,21 @@ function App() {
   );
 }
 
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
 export default App;
+
 
 
 
