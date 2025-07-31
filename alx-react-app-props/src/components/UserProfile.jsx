242167fa1e02a9b7dacee6c93d4a5de7cca5898f
suleboss <walesolih@gmail.com>
2025-07-31 22:10:53 +0100

    Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,22 @@
+/*import React from "react";
+
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;*/
+
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
+
