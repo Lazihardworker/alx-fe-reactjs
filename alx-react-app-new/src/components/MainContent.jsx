0a264ed1f4804519a3df3943f03c825dac611ea8
suleboss <walesolih@gmail.com>
2025-07-31 21:27:06 +0100

    Added inline styling to components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
   return (
     <main style={{ padding: '1rem', backgroundColor: '#f0f8ff' }}>
-      <p>Welcome to the list of my favorite cities around the world.</p>
+      <p>"I love to visit New York, Paris, and Tokyo."</p>
     </main>
   );
 }
