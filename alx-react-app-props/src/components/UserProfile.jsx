47307d0e1afa1b9c6cf43024394480a7f279d79c
suleboss <walesolih@gmail.com>
2025-07-31 22:17:28 +0100

    Updated prop drilling to use context API

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -12,11 +12,21 @@ const UserProfile = (props) => {
 
 export default UserProfile;*/
 
-import UserInfo from './UserInfo';
+import { useContext } from "react";
+import UserContext from "../UserContext";
 
-function ProfilePage() {
-  return <UserInfo />;
-}
+const UserProfile = () => {
+  const user = useContext(UserContext);
 
-export default ProfilePage;
+  return (
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{user.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{user.age}</span>
+      </p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+};
 
+export default UserProfile;
