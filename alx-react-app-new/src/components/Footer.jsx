c927dfcea9442a7c78160b52087a13c5b358140c
suleboss <walesolih@gmail.com>
2025-07-31 21:05:56 +0100

    Added inline styling to components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', padding: '1rem', textAlign: 'center' }}>
+      <p>&copy; 2025 Sulaiman Solih. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '1rem', backgroundColor: '#f0f8ff' }}>
+      <p>Welcome to the list of my favorite cities around the world.</p>
+    </main>
+  );
+}
+
+export default MainContent;
