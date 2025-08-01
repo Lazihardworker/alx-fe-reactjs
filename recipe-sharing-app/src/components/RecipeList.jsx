ecc9306c7f92c4c3219b6a6d5a6d5f19bbe9c87b
suleboss <walesolih@gmail.com>
2025-08-01 23:23:50 +0100

    Update recipe file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore'; // updated path
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
